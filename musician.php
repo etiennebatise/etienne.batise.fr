65deb7b71949000dc5f974e1e597178c2cd4db1b
Etienne Batise <ebatise@quantic-telecom.net>
2013-11-02 15:01:15 +0100

    Modifs contenu page musique

--- musician.php
+++ musician.php
@@ -14,13 +14,9 @@
 	<section>
 		<h1>Matress</h1>
 
-		<aside>
-			<!-- <img src="res/logoQT.png" alt="logo de Quantic Télécom"/> -->
-		</aside>
-		
 		<article>
 			<p>
-			Matress est un groupe de rock composé de 4 personnes, Pierre Gadeceau au chant et à la guitare, Léonard Levy à la guitare, Baptiste Lefebvre à la basse et moi-même à la batterie. Nous composons nos propres musiques et fesont également de nombreuses reprises de groupe comme the Rolling Stones, the White Stripes ou encore Juliette and The Licks our notre plaisir. Plusieurs concerts sont actuellements prévus. Deux maquettes sont disponible sur le compte SoundCloud de notre groupe. 
+			Matress est un groupe de rock composé de 4 personnes, Pierre Gadeceau au chant et à la guitare, Léonard Levy à la guitare, Baptiste Lefebvre à la basse et moi-même à la batterie. Nous composons nos propres musiques et fesont également de nombreuses reprises de groupes comme the Rolling Stones, the White Stripes ou encore Juliette and The Licks pour notre plaisir. Plusieurs concerts sont actuellements prévus. Deux maquettes sont disponible sur notre compte SoundCloud. 
 			</p>
 
 			<p><a href="#"></a>Écouter les maquettes sur SoundCloud</a></p>
@@ -30,13 +26,9 @@
 	<section>
 		<h1>PHI</h1>
 
-		<aside>
-			<img src="res/" alt=""/>
-		</aside>
-		
 		<article>
 			<p>
-			Dans le cadre de Phi, je suis responsable de la composition et de l'interprétation de la bande originale du jeu. Cela me permet, en dehors des aspect théorique, d'apprendre à créé de la musique sur un ordinateur, de découvrir les possibilités et les limites que nous imposent les différents outils disponibles et de travailler sur de nouveaux supports.
+			PHI est un projet de plusieurs étudiant dont le but est de réaliser un jeux vidéo. Je suis responsable de la composition et de l'interprétation de la bande originale du jeu. Cela me permet, en dehors des aspect théorique, d'apprendre à créé de la musique sur un ordinateur, de découvrir les possibilités et les limites que nous imposent les différents outils disponibles et de travailler sur de nouveaux supports.
 			</p>
 
 			<p><a href="#">Écouter la bande original du jeu</a></p>
@@ -46,26 +38,10 @@
 	<section>
 		<h1>Derniers articles sur le développement</h1>
 		
-		<aside>
-			<!-- <img src="res/vinyl2.jpg" alt="Image de vinyl"/> -->
-		</aside>
-		
 		<article>
 			Aucun articles pour le moment.
 		</article>
 	</section>
-
-<!-- 	<section id="student-frame">
-		<h1>Étudiant</h1>
-		<aside>
-			<img src="res/coffee.jpg">
-		</aside>
-		
-		<article>
-			<p></p>			
-		</article>
-
-	</section> -->
 
 	<footer>
 		<?php include("footer.php") ?>
