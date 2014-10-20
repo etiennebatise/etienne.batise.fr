0fb240381e1860e7c13b3e5cab1619564fc69c3c
etiennebatise <etienne.batise@gmail.com>
2014-10-20 12:37:42 +0300

    refactoring, better gestion of document height and addition of the contact part

--- js/app.js
+++ js/app.js
@@ -11,6 +11,13 @@ $.fn.easyTyped = function(tag) {
         var subsection = "#sectionHob";
         var subaside   = "#asideHob";
     }
+    else if(tag == "con") {
+        var target     = "#con";
+        var text       = "ls etienne/contact_options";
+        var subsection = "#sectionCon";
+        var subaside   = "null";
+    }
+
 
     $(target).typed({
         strings    : [text],
@@ -20,42 +27,53 @@ $.fn.easyTyped = function(tag) {
         callback   : function() {
             $(subsection).delay(500).show(0);
             setTimeout( function() {
-                $(subaside).css('visibility', 'visible');
+                if ( !(tag === "null") )
+                    $(subaside).css('visibility', 'visible');
             }, 500);
         }
     });
 };
 
-$.fn.triggerEvent = function(number) {
-    
-    // If the key is 'Enter' and we can still display things
-    if ((number < 2)) {
+$.fn.heightCss = function(coef) {
+    var newHeight = $(window).height() * coef;
+    var result    = newHeight.toString();
+    result        = result +"px";
+    return result;
+}
 
-        $('body').css('height', '200%');
+$.fn.triggerEvent = function(number) {
+
+    var viewport = $(document).height();
+    var scrollToSettings = {offsetTop : '20', easing : 'swing'};
+    // If the key is 'Enter' and we can still display things
+    if ((number < 3)) {
+
+        if ( number === 0 ) {
+            $(document).easyTyped("exp");
+        }
 
         // The first time, we display the cool stuff section
-        if( number === 0 ) {
+        if ( number === 1 ) {
+            $('body').css('height', $(document).heightCss(1.95));
             $('#secondSection').css('visibility', 'visible');
-            $('html, body').scrollTo('#secondSection');
+            $('html, body').scrollTo('#secondSection', scrollToSettings);
             $(document).easyTyped("hob");
-
         }
 
         // The second time we display the contact section
-        else if ( number === 1 ) {
-            alert('todo contact');
+        else if ( number === 2 ) {
+            $('body').css('height', $().heightCss(2.65));
+            $('#thirdSection').css('visibility', 'visible');
+            $('html, body').scrollTo('#thirdSection',scrollToSettings);
+            $(document).easyTyped("con");
+            /*
+             *setTimeout( function() { $('body').css('height', 'auto'); }, 6000);
+             */
+            
         }
-
         number++;
     }
 
-    // Then when it is over, we resize the body to make sure
-    // it's not possible to scroll again in the 
-    if (number === 2) {
-        setTimeout( function() {
-            $('body').css('height', 'auto');
-        }, 1900);
-    }
 
     return number;
 
@@ -63,25 +81,21 @@ $.fn.triggerEvent = function(number) {
 
 // Main js script
 $(function() {
-    // TODO Refactor this in the triggerEvent function
     // A counter of events triggered 
     var counter = 0;
-    var timeout =false;
+    var timeout = false;
+    var delay   = 1500;
 
     // First we trigger the first typed. It will display the section about my experience
-    $(document).easyTyped("exp");
-    setTimeout(function() {
-        timeout = true;
-    }, 2000);
+    counter = $(document).triggerEvent(counter);
+    setTimeout(function() { timeout = true; },delay);
 
     // Next the events are triggered by pressing enter or clicking on a "link"
     $(document).keypress(function(e) {
         if ((e.which == 13) && (timeout)) {
             counter = $(document).triggerEvent(counter);
             timeout = false;
-            setTimeout(function() {
-                timeout = true;
-            }, 1000);
+            setTimeout(function() { timeout = true; }, delay); 
         }
     });
 
