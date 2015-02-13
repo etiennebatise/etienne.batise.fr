5accb714b975fd6ea9f2c127279c335685a102ba
etiennebatise <etienne.batise@gmail.com>
2015-02-13 23:24:45 +0100

    Complete rewriting of app.js + few modifications every where

--- js/app.js
+++ js/app.js
@@ -3,53 +3,103 @@
  */
 $.fn.easyTyped = function(tag) {
 
-    // Experience section
-    if (tag === "exp") {
-        var text       = "cat daily_life";
-        var target     = "#exp";
-        var subsection = "#section-exp";
-        var subaside   = "#aside-exp";
-    }
-
-    // Hobbies section
-    else if(tag == "hob") {
-        var target     = "#hob";
-        var text       = "cat cool_stuff";
-        var subsection = "#section-hob";
-        var subaside   = "#aside-hob";
-    }
-
-    // Contact section
-    else if(tag == "con") {
-        var target     = "#con";
-        var text       = "ls contacts";
-        var subsection = "#section-con";
-        var subaside   = "null";
-    }
+    var state = 0;
+    if (state === 0 )
+        tags =  [ "#exp", "cat daily_life", "#section-exp", "#aside-exp", "#first-enter", "null" ];
 
 
-    // Abstrac call to the plugin 
-    $(target).typed({
-        strings    : [text],
+    if (state === 1 )
+        tags = [ "#hob", "cat cool_stuff", "#section-hob", "#aside-hob", "#second-enter", "#first-enter", ];
+
+
+    if (state === 2 )
+        tags = [ "#con", "ls contacts", "#section-con", "null", "null", "#second-enter", ];
+
+    alert(tags[1]);
+    $(tags[0]).typed({
+        strings    : [tags[1]],
         typeSpeed  : 0,
         startDelay : 1,
         showCursor : true,
         callback   : function() {
-            $(subsection).delay(100).css('visibility', 'visible');
-            // Delete that ?
-            setTimeout( function() {
-                if ( !(subaside === "null") )
-                    $(subaside).css('visibility', 'visible');
-            }, 500);
+            $(document).mShow(tags[2]);
+            $(document).mShow(tags[3]);
+            $(document).mShow(tags[4]);
+            //$(document).mHide(tags[5]);
         }
     });
+/*
+ *    // Experience section
+ *    if (tag === "exp") {
+ *        var text           = "cat daily_life";
+ *        var target         = "#exp";
+ *        var subsection     = "#section-exp";
+ *        var subaside       = "#aside-exp";
+ *        var enterToDisplay = "#first-enter";
+ *        var enterToHide    = "null";
+ *    }
+ *
+ *    // Hobbies section
+ *    else if(tag == "hob") {
+ *        var target         = "#hob";
+ *        var text           = "cat cool_stuff";
+ *        var subsection     = "#section-hob";
+ *        var subaside       = "#aside-hob";
+ *        var enterToDisplay = "#second-enter";
+ *        var enterToHide    = "#first-enter";
+ *    }
+ *
+ *    // Contact section
+ *    else if(tag == "con") {
+ *        var target         = "#con";
+ *        var text           = "ls contacts";
+ *        var subsection     = "#section-con";
+ *        var subaside       = "null";
+ *        var enterToDisplay = "null";
+ *        var enterToHide    = "#second-enter";
+ *    }
+ *
+ *    // We hide the last Enter button
+ *    // TODO Refactor outsie : this just deal with the type.
+ *    if ( !(enterToHide === "null") )
+ *        $(enterToHide).css('visibility', 'hidden');
+ *
+ *    // Abstrac call to the plugin 
+ *    $(target).typed({
+ *        // TODO refactor outside the function
+ *        strings    : [text],
+ *        typeSpeed  : 0,
+ *        startDelay : 1,
+ *        showCursor : true,
+ *        callback   : function() {
+ *
+ *            // refactor this function outside
+ *            // We display the Enter button
+ *            // for the  current title typed
+ *            //
+ *            if ( !(enterToDisplay === "null") )
+ *                $(enterToDisplay).css('visibility', 'visible');
+ *
+ *            // We display the text
+ *            $(subsection).delay(100).css('visibility', 'visible');
+ *
+ *            $(subaside).css('visibility', 'visible');
+ *            // We display the Next button
+ *            // Delete that ?
+ *            //setTimeout( function() {
+ *            //if ( !(subaside === "null") )
+ *            //$(subaside).css('visibility', 'visible');
+ *            //}, 500);
+ *        }
+ *    });
+ */
 };
 
 /*
  * Trigger the events corresponding to the thing to display.
  * This is called when <Enter> is pressed, or when the good element is clicked.
  */
-$.fn.triggerEvent = function(number, viewport) {
+$.fn.triggerEvent = function(number, viewport, callBackFunction) {
 
     // Default setting for the ScrollTo lib
     var scrollToSettings = {offsetTop : '3px', easing : 'swing', duration : '1s'};
@@ -60,6 +110,7 @@ $.fn.triggerEvent = function(number, viewport) {
     }
 
     // TODO there can be a way  to refactor the two following statements (arrays ?)
+    // And put here the enters buttons in here too
     // The second time, we display the cool stuff section
     if ( number === 1 ) {
 
@@ -80,7 +131,8 @@ $.fn.triggerEvent = function(number, viewport) {
     else if ( number === 2 ) {
 
         // We hide the seccond trigger "button"
-        $('#second_Trigger').css('visibility', 'hidden');
+        //$('#second_Trigger').css('visibility', 'hidden');
+        $(document).mShow('#second_Trigger');
 
         // We make visible the following section
         $('#third-section').css('visibility', 'visible');
@@ -97,9 +149,15 @@ $.fn.triggerEvent = function(number, viewport) {
         // We increment that amazingly named counter to know where we are
         number++;
     }
+
+    if (callBackFunction == "function") callBackFunction(number);
+
     return number;
 };
 
+$.fn.mShow = function(tag) {
+    $(tag).css('visibility', 'visible');
+}
 /*
  * Main js script
  */
@@ -115,20 +173,45 @@ $(function() {
         $('html, body').scrollTo('body');
     });
 
+
     // First we trigger the first typed. It will display the section about my experience
     counter = $(document).triggerEvent(counter);
     setTimeout(function() { timeout = true; },delay);
 
     // Next the events are triggered by pressing enter or clicking on a "link"
+    // Here we deal with the keyboard triggers
     $(document).keypress(function(e) {
         if ((e.which == 13) && (timeout)) {
+            // refactor this line
             counter = $(document).triggerEvent(counter, viewport);
             timeout = false;
             setTimeout(function() { timeout = true; }, delay); 
         }
     });
 
-    $('#aside-exp, #aside-hob').click(function() {
-            counter = $(document).triggerEvent(counter, viewport);
+    // Here we deal with the click triggers
+
+    var clickTarget = ['#aside-exp', '#aside-hob'];
+    $(clickTarget.toString()).click(function() {
+        // refactor this line
+        counter = $(document).triggerEvent(counter, viewport);
+        timeout = false;
+        setTimeout(function() { timeout = true; }, delay); 
+    });
+
+    // Here we deal with the scroll triggers
+    $(window).scroll(function(){
+        if ( ($("#second-section").visible(true, false)) && timeout ) {
+            // refactor this line
+            counter = $(document).triggerEvent(counter, viewport, function(counter) {
+                counter = $(document).triggerEvent(counter, viewport);
+            });
+            //counter = $(document).triggerEvent(counter, viewport);
+            //setTimeout(function() {
+                //counter = $(document).triggerEvent(counter, viewport);
+                //}, 600); 
+            timeout = false;
+            setTimeout(function() { timeout = true; }, delay); 
+        }
     });
 });
---
+++ js/new_app.js
@@ -0,0 +1,176 @@
+/*
+ * Main script
+ * Its jobs is to react to the different user interactions :
+ *   - Return Key pressed
+ *   - A click is made on the buttons
+ *   - The user is scrolling
+ *
+ * Difficulty : wait for the end of the section animations to enable the next animation.
+ */
+$(function() {
+
+    // The state is the number of section that are visible
+    var state = 0;
+
+    // The clickTargets are the tags the will update the state
+    var clickTargets = ['#aside-exp', '#aside-hob'];
+
+    // The hiddenSections are the section that need to be visible on the view port to enable the  update when the user scrolls
+    // The sections need to have their visibility set to hidden
+    var section = ['#second-section'];
+
+    // By default we consider the device is a wide screen
+    var isOnMobile = false;
+
+    // But we check thanks to media queries in css
+    if( $(".enter").css("display") == "none")
+        isOnMobile = true;
+
+
+    // We want the initial state to be one
+    state = $(document).update(state);
+
+
+    // When the return key is pressed
+    $(document).keypress(function(e) {
+
+        if  (e.which == 13)
+            state = $(document).update(state);
+
+    });
+
+
+    // When the clickTargets are clicked 
+    $(clickTargets.toString()).click(function() {
+
+        state = $(document).update(state);
+
+    });
+
+    // When the user scrolls
+
+    var scrollEventTimout = true;
+    //$(window).scroll(function() {
+        //// if the device is a wide screen
+        //if ( !isOnMobile ) {
+
+            //// if what we want to display is on the viewport
+            //if ( ($(section.toString()).visible(true, false)) && !updateLocked ) {
+                //// We trigger the 2 last states because I want it to behave this way for the moment
+                //state = $(document).update(state);
+                //scrollEventTimout = false;
+                //setTimeout(function() { scrollEventTimout = true; }, 500); 
+            //}
+
+        //}
+
+    //});
+});
+
+/*
+ * This function has to deal with all the things that need to be visible or hidden, the viewport movements.
+ * It also triggers the section animations and disable itself until they are complete.
+ *
+ * param : state is an int in [0, 1, 2]
+ */
+$.fn.update = function(state, callBack) {
+
+    var sections = ['first-section','second-section','third-section'];
+
+    /*
+     * For each updates, we want to :
+     *   0. Lock the function
+     *   1. Display the next section
+     *   2. Move To this section
+     *   3. Trigger the animation
+     *   4. Unlock the function
+     */
+
+    if (updateLocked === false) {
+        // Locker
+        updateLocked = true;
+
+        $(document).mShow("#" + sections[state]);
+
+        if (state != 0)
+            $(document).moveTo(sections[state]);
+
+        $(document).animateSection(state);
+
+        // Unlocker
+        updateLocked = false;
+
+        state = state + 1;
+    }
+
+    return state;
+
+};
+
+
+/*
+ * This function is just a layer for the scrollTo function
+ */
+$.fn.moveTo = function(tag) {
+
+    var scrollToSettings = {
+        offsetTop : '3px',
+        easing    : 'swing',
+        duration  : '1s'
+    };
+
+    var target = "#" + tag;
+    $('html, body').scrollTo(target, scrollToSettings);
+};
+
+$.fn.animateSection = function(state) {
+
+    var tags;
+
+    if (state === 0 )
+                //0        , 1                , 2              , 3            , 4              , 5
+        tags =  [ "#exp" , "cat daily_life" , "#section-exp" , "#aside-exp" , "#first-enter" , "null" ];
+
+
+    if (state === 1 )
+                //0     , 1                , 2              , 3            , 4               , 5              , 6
+        tags = [ "#hob" , "cat cool_stuff" , "#section-hob" , "#aside-hob" , "#second-enter" , "#first-enter" , "#aside-exp" ];
+
+
+    if (state === 2 )
+                //0     , 1             , 2              , 3      , 4      , 5               , 6
+        tags = [ "#con" , "ls contacts" , "#section-con" , "null" , "null" , "#second-enter" , "#aside-hob" ];
+
+    $(document).mHide(tags[5]);
+    $(document).mHide(tags[6]);
+
+    $(tags[0]).typed({
+        strings    : [tags[1]],
+        typeSpeed  : 0,
+        startDelay : 1,
+        showCursor : true,
+        callback   : function() {
+            $(document).mShow(tags[2]);
+            $(document).mShow(tags[3]);
+            $(document).mShow(tags[4]);
+        }
+    });
+
+};
+
+/*
+ * The global variable used to lock the update function
+ */
+var updateLocked = false;
+
+/*
+ * Utils 
+ */
+$.fn.mShow = function(tag) {
+    if (!(tag === "null"))
+        $(tag).css('visibility', 'visible');
+};
+$.fn.mHide = function(tag) {
+    if (!(tag === "null"))
+        $(tag).css('visibility', 'hidden');
+};
